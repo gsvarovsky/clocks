81845ac9e666db791a0dae38b1a990460c9e4283
George Svarovsky <gsvarovsky@gmail.com>
2019-02-10 06:32:12 +0000

    Refactoring

--- src/main/java/org/example/ConvergenceTest.java
+++ src/main/java/org/example/ConvergenceTest.java
@@ -1,39 +1,39 @@
 package org.example;
 
+import java.util.Collection;
 import java.util.Random;
-import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.CountDownLatch;
 
 public abstract class ConvergenceTest<P>
 {
-    private final Set<P> processes;
+    private final Collection<P> processes;
     protected final int iterationTarget, tickMillis;
 
     protected final Timer timer = new Timer(this.getClass().getSimpleName());
     protected final Random random = new Random(); // Happy with contention here
     protected final CountDownLatch done;
 
-    public ConvergenceTest(Set<P> processes, int iterationTarget, int tickMillis, int countsPerProxyTask)
+    public ConvergenceTest(Collection<P> processes, int iterationTarget, int tickMillis, int countsPerProcess)
     {
-        if (countsPerProxyTask < 1)
+        if (countsPerProcess < 1)
             throw new IllegalArgumentException("At least one count per process task");
 
         this.processes = processes;
         this.iterationTarget = iterationTarget;
         this.tickMillis = tickMillis;
-        this.done = new CountDownLatch(processes.size() * countsPerProxyTask * iterationTarget);
+        this.done = new CountDownLatch(processes.size() * countsPerProcess * iterationTarget);
     }
 
-    public ConvergenceTest(Set<P> processes, int countsPerProxyTask)
+    public ConvergenceTest(Collection<P> processes, int countsPerProcess)
     {
-        this(processes, 50, 10, countsPerProxyTask);
+        this(processes, 50, 10, countsPerProcess);
     }
 
     public void run() throws InterruptedException
     {
-        processes.forEach(p -> timer.schedule(new ProcessTask(p, 1), random.nextInt(tickMillis)));
+        processes.forEach(p -> timer.schedule(new ProcessTask(iteration(p), 1), random.nextInt(tickMillis)));
         done.await();
     }
 
@@ -41,14 +41,12 @@ public abstract class ConvergenceTest<P>
 
     private class ProcessTask extends TimerTask
     {
-        final P process;
         final Runnable iteration;
         final int i;
 
-        public ProcessTask(P process, int i)
+        public ProcessTask(Runnable iteration, int i)
         {
-            this.process = process;
-            this.iteration = iteration(process);
+            this.iteration = iteration;
             this.i = i;
         }
 
@@ -58,7 +56,7 @@ public abstract class ConvergenceTest<P>
             iteration.run();
 
             if (i < iterationTarget)
-                timer.schedule(new ProcessTask(process, i + 1), random.nextInt(tickMillis));
+                timer.schedule(new ProcessTask(iteration, i + 1), random.nextInt(tickMillis));
 
             done.countDown();
         }
